334d604fc6e4c2402ca429ec2bda8e2459ff488e
Paul Smith <code@uvwxy.de>
2014-02-18 20:02:51 +0100

    feature: add soundfinder

--- src/de/uvwxy/whereami/fragments/FragmentOverlaySupportMap.java
+++ src/de/uvwxy/whereami/fragments/FragmentOverlaySupportMap.java
@@ -2,21 +2,50 @@ package de.uvwxy.whereami.fragments;
 
 import java.util.ArrayList;
 
+import android.location.Location;
+
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import de.uvwxy.soundfinder.SoundFinder;
 import de.uvwxy.whereami.ActivityMain;
 import de.uvwxy.whereami.proto.Messages;
 
 public class FragmentOverlaySupportMap extends SupportMapFragment {
 	private GoogleMap mMapView = null;
+	private OnMarkerClickListener mMarkerClick = new OnMarkerClickListener() {
+
+		@Override
+		public boolean onMarkerClick(Marker marker) {
+			return false;
+		}
+	};
 
 	public void onViewCreated(android.view.View view, android.os.Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
 		mMapView = getMap();
+		mMapView.setOnMarkerClickListener(mMarkerClick);
+		mMapView.setOnMapLongClickListener(new OnMapLongClickListener() {
+
+			@Override
+			public void onMapLongClick(LatLng point) {
+				float dist = 0;
+				Location l = ActivityMain.mLastLocation;
+				if (l != null) {
+					Location t = new Location("");
+					t.setLatitude(point.latitude);
+					t.setLongitude(point.longitude);
+					dist = l.distanceTo(t);
+				}
+				SoundFinder.findNode(getActivity(), point.latitude, point.longitude, 0, 20, 30, dist);
+			}
+		});
 		loadMarkers(mMapView);
 	};
 
@@ -43,6 +72,7 @@ public class FragmentOverlaySupportMap extends SupportMapFragment {
 			} else {
 				options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
 			}
+			options.title(l.getName());
 			map.addMarker(options);
 		}
 
